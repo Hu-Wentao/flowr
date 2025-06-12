fc3d5c6a1f97a111fbd32bc0a4a416755c983b6a
WyattHu(胡文涛) <wyatt.hu@hisunglobal.com>
2025-06-12 20:54:34 +0800

    feat FrReadContext onlyGlobal can set null; mean global first

--- lib/src/mvvm/ext.dart
+++ lib/src/mvvm/ext.dart
@@ -7,21 +7,34 @@ import 'package:get_it/get_it.dart' show GetIt;
 import 'package:provider/provider.dart' show Provider;
 
 extension FrReadContext on BuildContext {
-  T read<T extends FrViewModel>({bool onlyGlobal = false}) {
-    if (onlyGlobal) return readGlobal<T>()!;
-    try {
-      return Provider.of<T>(this, listen: false);
-    } catch (e) {
-      final r = readGlobal<T>(nothrow: true);
-      if (r != null) return r;
-      rethrow;
+  /// [onlyGlobal]
+  ///   true: only read Global;
+  ///   null: read Global first, then Provider;
+  ///   false: read Provider first, then Global
+  T read<T extends FrViewModel>({bool? onlyGlobal = false}) {
+    if (onlyGlobal == null) {
+      // global -> provider
+      return readGlobal(nothrow: true) ?? Provider.of<T>(this, listen: false);
+    } else if (onlyGlobal) {
+      // global!
+      return readGlobal<T>()!;
+    } else {
+      // provider -> global
+      try {
+        return Provider.of<T>(this, listen: false);
+      } catch (e) {
+        final r = readGlobal<T>(nothrow: true);
+        if (r != null) return r;
+        rethrow;
+      }
     }
   }
 
   T? readGlobal<T extends FrViewModel>({bool nothrow = false}) {
     if (GetIt.I.isRegistered<T>()) {
       final r = GetIt.I.get<T>();
-      log('FrReadContext get Global <$T>[#${shortHash(r)}] ${r.valueOrNull} ', name: 'FlowR');
+      log('FrReadContext get Global <$T>[#${shortHash(r)}] ${r.valueOrNull} ',
+          name: 'FlowR');
       return r;
     }
     if (nothrow) return null;
