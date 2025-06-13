80b002edf3a91a84e6d52fa594291cef86f8d830
WyattHu(胡文涛) <wyatt.hu@hisunglobal.com>
2025-06-13 14:35:38 +0800

    doc base.dart

--- lib/src/base.dart
+++ lib/src/base.dart
@@ -4,6 +4,7 @@ import 'package:rxdart/rxdart.dart';
 
 /// 最基础的 [BaseFlowR]
 abstract class BaseFlowR<T> {
+  /// core stream controller
   StreamController<T> get subject;
 
   /// 通过覆写[onCreate],可以实现在首次创建[subject]时设置初始数据[T]实例
@@ -11,18 +12,22 @@ abstract class BaseFlowR<T> {
       ? BehaviorSubject<T>.seeded(initValue)
       : BehaviorSubject<T>();
 
+  /// put new value
   BaseFlowR<T> put(T value) {
     subject.add(value);
     return this;
   }
 
+  /// put new error
   BaseFlowR<T> putError(Object error, [StackTrace? stackTrace]) {
     subject.addError(error, stackTrace);
     return this;
   }
 
+  /// get value's stream
   Stream<T> get stream => subject.stream;
 
+  /// get current value
   T get value => (subject as BehaviorSubject<T>).value;
 
   /// 如果没有初始值, 则[value]可能为null,使用[valueOrNull]避免抛出异常
