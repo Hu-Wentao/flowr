a0f45d89c0f3f6195fcfe7696bf0c81e7b6ee3c8
WyattHu(胡文涛) <wyatt.hu@hisunglobal.com>
2025-07-04 09:28:44 +0800

    feat FrReadContextX can read any type

--- lib/src/mvvm/ext.dart
+++ lib/src/mvvm/ext.dart
@@ -6,34 +6,38 @@ import 'package:flutter/widgets.dart' show BuildContext;
 import 'package:get_it/get_it.dart' show GetIt;
 import 'package:provider/provider.dart' show Provider;
 
-extension FrReadContext on BuildContext {
+extension FrReadContextX on BuildContext {
   /// [onlyGlobal]
   ///   true: only read Global;
   ///   null: read Global first, then Provider;
   ///   false: read Provider first, then Global
-  T read<T extends FrViewModel>({bool? onlyGlobal = false}) {
+  T read<T extends Object>({bool? onlyGlobal = false}) {
+    if (T is! FrViewModel) return Provider.of<T>(this, listen: false);
     if (onlyGlobal == null) {
       // global -> provider
       return readGlobal(nothrow: true) ?? Provider.of<T>(this, listen: false);
     } else if (onlyGlobal) {
       // global!
-      return readGlobal<T>()!;
+      return _readGlobal<T>()!;
     } else {
       // provider -> global
       try {
         return Provider.of<T>(this, listen: false);
       } catch (e) {
-        final r = readGlobal<T>(nothrow: true);
+        final r = _readGlobal<T>(nothrow: true);
         if (r != null) return r;
         rethrow;
       }
     }
   }
 
-  T? readGlobal<T extends FrViewModel>({bool nothrow = false}) {
+  T? readGlobal<T extends FrViewModel>({bool nothrow = false}) =>
+      _readGlobal(nothrow: nothrow);
+
+  T? _readGlobal<T extends Object>({bool nothrow = false}) {
     if (GetIt.I.isRegistered<T>()) {
       final r = GetIt.I.get<T>();
-      log('FrReadContext get Global <$T>[#${shortHash(r)}] ${r.value} ',
+      log('FrReadContext get Global <$T>[#${shortHash(r)}] ${(r is FrViewModel) ? r.value : ''} ',
           name: 'FlowR');
       return r;
     }
