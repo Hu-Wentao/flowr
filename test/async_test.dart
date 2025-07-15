e06d6ef92d78594ceee585f1968b240c76ce1c1e
WyattHu(胡文涛) <wyatt.hu@hisunglobal.com>
2025-07-15 11:39:38 +0800

    test async_test.dart;tic_test.dart

--- test/async_test.dart
+++ test/async_test.dart
@@ -52,4 +52,95 @@ main() {
       print('--- End ---');
     });
   });
+
+  group('FutureOr test', () {
+    test('await FutureOr', () async {
+      FutureOr<int> foo() => 1;
+      FutureOr<int> foo2() =>
+          Future.delayed(const Duration(milliseconds: 100)).then((value) => 2);
+      FutureOr<int> foo3() async =>
+          Future.delayed(const Duration(milliseconds: 100)).then((value) => 2);
+      FutureOr<int> foo4() async =>
+          await Future.delayed(const Duration(milliseconds: 100))
+              .then((value) => 2);
+      print('start');
+      final f1 = await foo();
+      print('f1: $f1');
+      final f2 = await foo2();
+      print('f2: $f2');
+      final f3 = await foo3();
+      print('f3: $f3');
+      final f4 = await foo4();
+      print('f4: $f4');
+      print('end');
+    });
+    test('FutureOr', () async {
+      FutureOr<int> foo1() => 1;
+      FutureOr<int> foo12() async => 1;
+      FutureOr<int> foo13() async => await 1;
+
+      FutureOr<int> foo2() =>
+          Future.delayed(const Duration(milliseconds: 100)).then((value) => 2);
+      FutureOr<int> foo3() async =>
+          Future.delayed(const Duration(milliseconds: 100)).then((value) => 2);
+      FutureOr<int> foo4() async =>
+          await Future.delayed(const Duration(milliseconds: 100))
+              .then((value) => 2);
+      print('start');
+      final f1 = foo1();
+      print('f1: $f1');
+      final f12 = foo12();
+      print('f12: $f12');
+      final f13 = foo13();
+      print('f13: $f13');
+      final f2 = foo2();
+      print('f2: $f2');
+      final f3 = foo3();
+      print('f3: $f3');
+      final f4 = foo4();
+      print('f4: $f4');
+      print('end');
+    });
+  });
+
+  group('test updateRaw', () {
+    FutureOr<void> updateRaw<T>(
+      FutureOr<T> Function(T value) update, {
+      Function(Object e, StackTrace s)? onError,
+      required T debugValue,
+    }) async {
+      final value = debugValue;
+      put(v) {
+        print('put value $v');
+        return v;
+      }
+      putError(v, s) {
+        print('putError value $v');
+        return v;
+      }
+      try {
+        final data = update(value);
+        if (data is Future<T>) {
+          await data.then(put);
+        } else {
+          put(data);
+        }
+      } catch (e, s) {
+        onError?.call(e, s);
+        if (onError == null) putError(e, s);
+      }
+    }
+
+    test('updateRaw', () {
+      print('start');
+      updateRaw<int>(
+        (a) {
+          print('do...');
+          return a * 2;
+        },
+        debugValue: 1,
+      );
+      print('end');
+    });
+  });
 }
---
+++ test/tic_test.dart
@@ -0,0 +1,52 @@
+import 'package:flowr/flowr_mvvm.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class FooViewModel extends FrViewModel<int> {
+  @override
+  frPrint(String message,
+      {DateTime? time,
+      int? sequenceNumber,
+      int? level,
+      String? name,
+      Zone? zone,
+      Object? error,
+      StackTrace? stackTrace}) {
+    return print('$name] $message');
+  }
+
+  @override
+  final int initValue = 0;
+  late final stmTic = stream.switchMap((e) {
+    logger('stmTic $e');
+    if (e == 0) return Stream.fromIterable([]);
+    return Stream.periodic(Duration(seconds: e), (_) => _);
+  });
+
+  FooViewModel() {
+    autoDispose(stmTic
+        .listen((event) => logger('call # ${DateTime.now()} $value; $event')));
+  }
+
+  start() => update((old) => old = 1);
+
+  stop() => update((old) => old = 0);
+}
+
+main() {
+  test('logger (appendWith)', () async {
+    print("run");
+    final f = FooViewModel();
+    print("wait ...");
+    await Future.delayed(Duration(seconds: 3));
+    print("run start ${f.value}");
+    await f.start();
+    await f.start(); // start 2
+    await Future.delayed(Duration(seconds: 5));
+    print("run stop # ${DateTime.now()}; ${f.value}");
+    await f.start(); // start 3
+    await Future.delayed(Duration(seconds: 3));
+    await f.stop();
+    await Future.delayed(Duration(seconds: 3));
+    print("end # ${DateTime.now()}; ${f.value}");
+  });
+}
