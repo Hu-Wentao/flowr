12c0d06a9aea16f76c0a82f6a2e528c940c3462d
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2025-07-19 00:50:29 +0800

    refactor MapValueX,DistinctByValueX feat _MapValueStream,_DistinctValueStream

---
+++ test/value_stream_test.dart
@@ -0,0 +1,27 @@
+import 'package:flowr/flowr_mvvm.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  group('value stream', () {
+    test('stream', () async {
+      final s = Stream.fromIterable([1, 2, 3, 4]);
+      final vs = s.shareValueSeeded(-1);
+      var count = 0;
+      await vs.listen((e) {
+        print(e);
+        count++;
+      }).asFuture();
+      expect(count, 5);
+    });
+    test('mapValue stream', () async {
+      final s = Stream.fromIterable([1, 2, 3, 4]);
+      final vs = s.shareValueSeeded(-1);
+      var count = 0;
+      await vs.mapValue((e) => e * 2).listen((e) {
+        print(e);
+        count++;
+      }).asFuture();
+      expect(count, 5);
+    });
+  });
+}
