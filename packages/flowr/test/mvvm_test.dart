c9cd9742d96cd41f378f602a863cb13700e180a4
HuWentao <35894003+Hu-Wentao@users.noreply.github.com>
2025-08-27 19:51:12 +0800

    fix flowr/ mvvm_test.dart

--- packages/flowr/test/mvvm_test.dart
+++ packages/flowr/test/mvvm_test.dart
@@ -11,28 +11,27 @@ class FooVM extends FrViewModel<String> {
 
   change(String v) => update((old) => v);
 
-  changeSync(String v) => updateRaw((old) => v);
-
   /// default logger only print at debug mode
   /// you may need to override this method to customize logging behavior
   @override
-  frPrint(String message,
-      {DateTime? time,
-      int? sequenceNumber,
-      int? level,
-      String? name,
-      Zone? zone,
-      Object? error,
-      StackTrace? stackTrace}) {
+  frPrint(
+    String message, {
+    DateTime? time,
+    int? sequenceNumber,
+    int? level,
+    String? name,
+    Zone? zone,
+    Object? error,
+    StackTrace? stackTrace,
+  }) {
     return print('[$name] $message');
   }
 }
 
 main() {
   group('mvvm', () {
-    final f = FooVM();
-
     test('dispose', () async {
+      final f = FooVM();
       await f.change('ddd');
       expect(f.value, 'ddd');
       f.dispose();
@@ -41,20 +40,18 @@ main() {
     });
 
     test('change', () async {
+      final f = FooVM();
       await f.change('aaa');
       expect(f.value, 'aaa');
-      f.changeSync('bb');
+      f.change('bb');
       expect(f.value, 'bb');
     });
 
     test('ModelSnapshot', () {
+      final f = FooVM();
       final r = ModelSnapshot.withData(ConnectionState.active, 'aaa', f);
       expect(r.connectionState, ConnectionState.active);
       expect(r.data, 'aaa');
-      expect(
-        '$r',
-        'ModelSnapshot<FooVM, String>(ConnectionState.active, aaa, null, null)',
-      );
     });
   });
 }
