45fd93fd72f273ea12e3bfe5ecd86fe5fba60be6
HuWentao <35894003+Hu-Wentao@users.noreply.github.com>
2026-02-21 22:56:11 +0800

    feat: add switchMap test for StmTest class

---
+++ packages/flowr_dart/test/switch_map_test.dart
@@ -0,0 +1,38 @@
+import 'package:flowr_dart/flowr_dart.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:test/test.dart';
+
+class StmTest extends FlowR {
+  @override
+  final int initValue;
+
+  StmTest({required this.initValue}) {
+    autoDispose(
+      stream
+          .distinctBy((e) => e)
+          .switchMap((e) {
+            // print('debug switchMap $e ${DateTime.now()}');
+            return e % 2 != 0
+                ? Stream.empty() // empty will not trigger onData
+                : Stream.periodic(Duration(seconds: 1), (_) => e);
+          })
+          .listen(onData),
+    );
+  }
+
+  void onData(event) {
+    print('debug onData $event ${DateTime.now()}');
+  }
+}
+
+main() async {
+  test('switchMap', () async {
+    final vm = StmTest(initValue: 1);
+    print('start');
+    await vm.update((o) => 66);
+    await Future.delayed(Duration(seconds: 2));
+    await vm.update((o) => 1);
+    print('end');
+    expect(vm.value, 1);
+  });
+}
