d6969850b9deaf875c7caa909c9429e5354e3060
HuWentao <35894003+Hu-Wentao@users.noreply.github.com>
2026-02-21 19:48:03 +0800

    remove flowr_arch

--- packages/flowr_arch/lib/flowr_arch.dart
+++
@@ -1,6 +0,0 @@
-export 'package:flowr_arch/src/repo.dart';
-export 'package:flowr_arch/src/fr_box.dart';
-
-/// FlowR-Arch
-///   IStorageConfig, ITable, IRepo
-///   FrStorageConfig, FrTable, FrRepo
