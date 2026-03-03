bba0c4e41706a9f3dcace69517d723050927f726
HuWentao <35894003+Hu-Wentao@users.noreply.github.com>
2026-03-03 15:53:27 +0800

    test

---
+++ packages/flowr/test/multi_update_test.dart
@@ -0,0 +1,32 @@
+import 'package:flowr/flowr_mvvm.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class FooVM extends FrViewModel<String> with TestLoggableMx {
+  @override
+  LogExtra? get logExtra => LogExtra.self;
+
+  @override
+  String get initValue => 'foo';
+
+  change(String v) => update((old) => v);
+}
+
+main() {
+  group('mvvm', () {
+    test('change', () async {
+      final f = FooVM();
+      await f.change('aaa');
+      expect(f.value, 'aaa');
+      f.change('bb');
+      expect(f.value, 'bb');
+    });
+
+    test('ModelSnapshot', () {
+      final f = FooVM();
+      final r = ModelSnapshot.withData(ConnectionState.active, 'aaa', f);
+      expect(r.connectionState, ConnectionState.active);
+      expect(r.data, 'aaa');
+    });
+  });
+}
