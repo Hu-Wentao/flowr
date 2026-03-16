a0cc0c2381c7051ba1df2132219d2dbd84a0eec7
HuWentao <35894003+Hu-Wentao@users.noreply.github.com>
2026-03-16 15:22:09 +0800

    test di

---
+++ packages/flowr/test/repro_di_test.dart
@@ -0,0 +1,31 @@
+import 'package:flowr/flowr_mvvm.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+class FooVM extends FrViewModel<String> {
+  @override
+  String get initValue => 'foo';
+}
+
+void main() {
+  testWidgets('FrProvider.di should pull from GetIt by default', (
+    tester,
+  ) async {
+    final getIt = GetIt.I;
+    getIt.registerLazySingleton<FooVM>(() => FooVM());
+
+    await tester.pumpWidget(
+      MultiProvider(providers: [FrProvider<FooVM>.di()], child: Container()),
+    );
+
+    final context = tester.element(find.byType(Container));
+
+    // This should not throw if FrProvider.di worked correctly
+    final vm = Provider.of<FooVM>(context, listen: false);
+    expect(vm, isA<FooVM>());
+    expect(vm.value, 'foo');
+
+    getIt.reset();
+  });
+}
